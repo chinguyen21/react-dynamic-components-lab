a82f0439e199eec93834c83daa5e4ec5d83abf93
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-02 00:08:28 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,13 +8,21 @@ export default class ColorBox extends Component {
     ]
   }
 
-  render() {
+  
+    render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: 1}}>
+        {this.props.opacity >= 0.2 ? (<ColorBox opacity={this.props.opacity-0.1}/>) : null}
       </div>
-    )
+    ) }
   }
 
-}
 
+  // function ColorBox(props) {
+  //   return (
+  //     <div className="color-box" style={{opacity: 0.5 /*replace null with the value*/}}>
+  //       {props.opacity >= 0.2 ? (<ColorBox opacity={props.opacity-0.1}/>) : null}
+  //     </div>
+  //   )
+  // }
+// export default ColorBox
